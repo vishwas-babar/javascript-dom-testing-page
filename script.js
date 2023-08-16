078060265b39af1f8f910a8d492f524b444b63c1
vishwas-babar <vishwasbabar886@gmail.com>
2023-08-16 16:49:31 +0000

    pushing dom testing page

--- script.js
+++ script.js
@@ -3,58 +3,58 @@
 
 
 // this part is for changig a padding and reseting it
-var source_padding=document.querySelector('div').querySelector('div').querySelectorAll('div');
-  padding_value=window.getComputedStyle(source_padding[0]).getPropertyValue('padding');
+var source=document.querySelector('div').querySelector('div').querySelectorAll('div'); // we can use this source for all dynamical operations
+
+
+ var padding_value=window.getComputedStyle(source[0]).getPropertyValue('padding');
 
 function changePadding() {
 var padding_input=document.getElementById("padding_input").value;
-
-var source_padding=document.querySelector("div").querySelector("div").querySelectorAll("div");
   
-  for (let i = 0; i < source_padding.length; i++) {
-    source_padding[i].style.padding=padding_input;
+  for (let i = 0; i < source.length; i++) {
+    source[i].style.padding=padding_input;
   }
 }
 
 function resetPadding() {
 
 
-  for (let i = 0; i < source_padding.length; i++) {
-    source_padding[i].style.padding=padding_value;
+  for (let i = 0; i < source.length; i++) {
+    source[i].style.padding=padding_value;
   }
   console.log("reset padding worked");
 }
 
 
 // background-color
-var source_backg=document.querySelector("div").querySelector("div").querySelectorAll("div");
-var backg_value=window.getComputedStyle(source_backg[0]).getPropertyValue('backgroundColor');
+
+var backg_value=window.getComputedStyle(source[0]).getPropertyValue('backgroundColor');
 
 function changeBackgroundColor() {
   console.log("change backgroundcolor working");
   let input_value=document.getElementById("backg_input").value;
-  for (let i = 0; i < source_backg.length; i++) {
-    source_backg[i].style.backgroundColor=input_value;
+  for (let i = 0; i < source.length; i++) {
+    source[i].style.backgroundColor=input_value;
   }
 }
 
 function resetBackgroundColor() {
-  for (let i = 0; i < source_backg.length; i++) {
-    source_backg[i].style.backgroundColor=backg_value;
+  for (let i = 0; i < source.length; i++) {
+    source[i].style.backgroundColor=backg_value;
   }
 }
 
 
 
 // change border
-var source_border=document.querySelector("div").querySelector("div").querySelectorAll("div");
-var border_value=window.getComputedStyle(source_border[0]).getPropertyValue('border');
+
+var border_value=window.getComputedStyle(source[0]).getPropertyValue('border');
 function changeBorder() {
   console.log("changeBorder(); working");
   let input_value=document.getElementById("border_input").value;
 
-  for (let i=0; i<source_border.length; i++) {
-    source_border[i].style.border=input_value;
+  for (let i=0; i<source.length; i++) {
+    source[i].style.border=input_value;
   }
   
 }
@@ -63,7 +63,25 @@ function changeBorder() {
 function resetBorder() {
   console.log("resetBorder() called");
 
-  for(let i=0; i<source_border.length; i++) {
-    source_border[i].style.border=border_value;
+  for(let i=0; i<source.length; i++) {
+    source[i].style.border=border_value;
   }
+}
+
+
+//change justify-content
+var source_justify=document.querySelector('div').querySelector('div');
+var justify_value=window.getComputedStyle(source_justify).getPropertyValue('justify-content');
+
+function changeJustifyContent() {
+  console.log("changeJustifyContent() called");
+  let input_value=document.getElementById('justify-content').value;
+
+  source_justify.style.justifyContent=input_value;
+}
+
+//reset justify-content
+function resetJustifyContent() {
+  console.log("resetJustifyContent() called");
+  source_justify.style.justifyContent=justify_value;
 }
